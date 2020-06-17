dd8dc07d308ff58ea155d3bbca6b420a498f2dee
nadare <nadaretakeshi@yahoo.co.jp>
2020-06-18 00:32:00 +0900

    magic を　探せ

--- feature/rolling_feat.py
+++ feature/rolling_feat.py
@@ -26,11 +26,15 @@ class Rolling_id(Feature):
 
         # id
         feat[f"id_lag_{LAG}_rmean_1"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG))
+        feat[f"id_lag_{LAG+1}_rmean_1"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG+1))
+        feat[f"id_lag_{LAG+2}_rmean_1"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG+2))
         feat[f"id_lag_{LAG}_rmean_7"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(7).mean())
         feat[f"id_lag_{LAG}_rmean_28"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(28).mean())
         feat[f"id_lag_{LAG}_rmean_63"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(63).mean())
         feat[f"id_lag_{LAG}_rmean_91"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(91).mean())
         feat[f"id_lag_{LAG}_rmean_182"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(182).mean())
+
+        feat[f"id_lag_{LAG}_rmean_rate_7_28"] = feat[f"id_lag_{LAG}_rmean_7"] / (feat[f"id_lag_{LAG}_rmean_28"] + 1e-9)
 
         feat[f"id_lag_{LAG}_rstd_28"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(28).std())
         feat[f"id_lag_{LAG}_rskew_28"] = data_df.groupby("id")["target"].transform(lambda x: x.shift(LAG).rolling(28).skew())
@@ -55,8 +59,18 @@ class Rolling_id(Feature):
                                     aggfunc="mean").shift(-(-LAG//7))
         res = pivot.rolling(4).mean().unstack().reset_index().rename({0: f"id_lag_{-(-LAG//7)}w_rmean_4w"}, axis=1)
         datas.append(data_df[["id", "dp7", "dd7"]].merge(res, on=["id", "dp7", "dd7"], how="left").drop(["id", "dp7", "dd7"], axis=1).astype(np.float32))
+        res = pivot.rolling(8).mean().unstack().reset_index().rename({0: f"id_lag_{-(-LAG//7)}w_rmean_8w"}, axis=1)
+        datas.append(data_df[["id", "dp7", "dd7"]].merge(res, on=["id", "dp7", "dd7"], how="left").drop(["id", "dp7", "dd7"], axis=1).astype(np.float32))
         res = pivot.rolling(12).mean().unstack().reset_index().rename({0: f"id_lag_{-(-LAG//7)}w_rmean_12w"}, axis=1)
         datas.append(data_df[["id", "dp7", "dd7"]].merge(res, on=["id", "dp7", "dd7"], how="left").drop(["id", "dp7", "dd7"], axis=1).astype(np.float32))
+
+        pivot = data_df.pivot_table(columns=["item_id", "dp7"],
+                                    index="dd7",
+                                    values="target",
+                                    aggfunc="mean").shift(-(-LAG//7))
+
+        res = pivot.rolling(4).mean().unstack().reset_index().rename({0: f"item_lag_{-(-LAG//7)}w_rmean_4w"}, axis=1)
+        datas.append(data_df[["item_id", "dp7", "dd7"]].merge(res, on=["item_id", "dp7", "dd7"], how="left").drop(["item_id", "dp7", "dd7"], axis=1).astype(np.float32))
 
         # item_id
         feat = pd.DataFrame()
@@ -96,7 +110,10 @@ class Rolling_id(Feature):
                                                         on= id_col + ["d"],
                                                         how="left").drop(id_col + ["d"], axis=1).astype(np.float16))
             collect()
-
+        
+        data_df.loc[:, "store_dept_simple_mean"] = data_df.groupby(["store_id", "dept_id", "d"])["target"].transform("mean")
+        data_df.loc[:, f"store_dept_simple_mean_lag_{LAG}"] = data_df.groupby(["id"])["store_dept_simple_mean"].transform(lambda x: x.shift(LAG))
+        datas.append(data_df[[f"store_dept_simple_mean_lag_{LAG}"]])
         self.data = pd.concat(datas, axis=1).astype(np.float32)
 
 if __name__ == "__main__":
